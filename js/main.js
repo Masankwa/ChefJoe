0da3a363cf63effb3dae4b6db6d7241b77df6678
Masankwa <you@example.com>
2024-11-09 20:58:19 +0200

    correction 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.querySelector('.hamburger-button');
     const mobileMenu = document.querySelector('.mobile-menu');
 
-    hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+    // Toggle mobile menu when hamburger button is clicked
+    hamburgerButton.addEventListener('click', (event) => {
+        event.stopPropagation();  // Prevent click from bubbling to document
+        mobileMenu.classList.toggle('active');
+    });
+
+    // Toggle mobile menu on when clicking anywhere if it's not active
+    document.addEventListener('click', (event) => {
+        if (!mobileMenu.classList.contains('active')) {
+            mobileMenu.classList.add('active');
+        }
+    });
 });
